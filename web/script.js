710b6d6f1ebb80db1b52aa4605b137525c5bcefa
martaku0 <marta.l.kurowska@gmail.com>
2023-06-13 01:48:49 +0200

    second

--- web/script.js
+++ web/script.js
@@ -1,4 +1,8 @@
 var rotate = false
+var current_element = "cube"
+var a = 0
+var col = 'red'
+var all_elements = ["cube"]
 
 window.addEventListener("load", () => {
     const ws = new WebSocket('ws://192.168.18.23:1337');
@@ -19,11 +23,13 @@ window.addEventListener("load", () => {
             z: data[2]
         }
 
-        if (rotate) {
-            document.getElementById('cube').style.transform = `rotate(${xyz.z * 100}deg)`
-        } else {
-            document.getElementById('cube').style.left = 500 + xyz.x * (-1000) + "px"
-            document.getElementById('cube').style.top = 500 + xyz.y * 1000 + "px"
+        if (document.getElementById(current_element)) {
+            if (rotate) {
+                document.getElementById(current_element).style.transform = `rotate(${xyz.z * 100}deg)`
+            } else {
+                document.getElementById(current_element).style.left = 500 + xyz.x * (-1000) + "px"
+                document.getElementById(current_element).style.top = 500 + xyz.y * 1000 + "px"
+            }
         }
     }
 
@@ -38,4 +44,67 @@ window.addEventListener("load", () => {
 
 function change() {
     rotate = !rotate
+}
+
+function stop_here() {
+    current_element = ""
+}
+
+const isColor = (strColor) => {
+    const s = new Option().style;
+    s.color = strColor;
+    return s.color !== '';
+}
+
+function isNumber(value) {
+    return !isNaN(value);
+}
+
+function new_cube() {
+    stop_here()
+    current_element = prompt("Podaj nazwę")
+    if (current_element === null) {
+        return;
+    }
+    while (all_elements.includes(current_element)) {
+        current_element += "-copy"
+    }
+
+    a = prompt("Podaj bok")
+    if (a === null) {
+        return;
+    }
+    while (isNaN(a) || a <= 0) {
+        a = prompt("Błędne wymiary. Podaj bok")
+    }
+
+    col = prompt("Podaj kolor")
+    if (col === null) {
+        return;
+    }
+    while (!isColor(col)) {
+        col = prompt("Błędny kolor. Wybierz coś innego")
+    }
+
+    all_elements.push(current_element)
+
+    var elemDiv = document.createElement('div');
+    document.body.appendChild(elemDiv);
+    elemDiv.id = current_element
+    elemDiv.style.backgroundColor = col
+    elemDiv.style.width = a + "px"
+    elemDiv.style.height = a + "px"
+}
+
+function clear_all() {
+    if (confirm("Jesteś pewien?")) {
+        let cubes = document.querySelectorAll('div')
+        cubes.forEach(element => {
+            element.remove()
+        });
+    }
+}
+
+function del_last() {
+    document.getElementById(`${all_elements[all_elements.length-1]}`).remove()
 }
